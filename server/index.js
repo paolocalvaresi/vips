887b52d8e7ce0d6c6eec553588705ccd7534d097
Paolo Calvaresi <paolo.calvaresi.v@gmail.com>
2018-06-25 15:23:18 +0200

    start di angular

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
 const PORT = 3000;
 
 require('./config/db.js')
@@ -7,5 +9,10 @@ require('./config/db.js')
 var routes = require('./routes');
 routes(app, express);
 
+
+// livereload
+var livereload = require('livereload');
+var server = livereload.createServer();
+server.watch(path.join(__dirname, '..', 'public'));
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
