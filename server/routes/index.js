970df6d3e4742a142da3b9512058476056a7519d
Paolo Calvaresi <paolo.calvaresi.v@gmail.com>
2018-06-25 13:00:03 +0200

    angular

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,13 +4,22 @@ module.exports = function (app, express) {
     // parsing del json
     app.use(express.json());
 
+    //servo i file statici
+    app.use('/angular', express.static(path.join(__dirname, '..', '..', 'node_modules', 'angular')));
+    app.use('/jquery', express.static(path.join(__dirname, '..', '..', 'node_modules', 'jquery', 'dist')));
+    app.use('/popper', express.static(path.join(__dirname, '..', '..', 'node_modules', 'popper.js', 'dist', 'umd')));
+    app.use('/bootstrap', express.static(path.join(__dirname, '..', '..', 'node_modules', 'bootstrap', 'dist')));
+
+    // servo la mia applicazione
+    app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+
     // servo la homepage
     app.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html')))
 
 
     // gestione delle rotte relative agli utenti
     app.use('/utenti', require('./utenti/index.js'));
-   
+
     // gestione delle rotte relative ai posts
     app.use('/posts', require('./posts/index.js'));
 
